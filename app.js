6bcf211aab2c32ec34af4f09406ed6c6f5124b43
J. Nicolas Aristizabal R <josenicolasaristizabalramirez@gmail.com>
2023-09-18 21:12:00 -0500

    add query selector

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+const pResult = document.querySelector("#p-result");
+console.log(pResult);
 const number = prompt("usuario ingrese un numero aquí !");
 console.log(number);
+
+pResult.innerHTML = `el numero ingresado es ${number}`;
